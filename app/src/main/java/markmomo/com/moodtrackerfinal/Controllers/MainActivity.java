2fe4719a289bbd494c481716d998199025aa7156
markmomo <markmomo.mm@gmail.com>
2019-07-20 17:45:42 +0200

    AlarmBroadCastReceiver & startAlarm() add AlarmBroadCastReceiver to track when the current day has changed

--- app/src/main/java/markmomo/com/moodtrackerfinal/Controllers/MainActivity.java
+++ app/src/main/java/markmomo/com/moodtrackerfinal/Controllers/MainActivity.java
@@ -1,13 +1,20 @@
 package markmomo.com.moodtrackerfinal.Controllers;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import java.util.Calendar;
+
 import markmomo.com.moodtrackerfinal.R;
+import markmomo.com.moodtrackerfinal.Tools.AlarmBroadcastReceiver;
 import markmomo.com.moodtrackerfinal.Tools.MoodsFragmentPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         mHistoryIcon = findViewById(R.id.act_main_history_icon);
 
         this.configureViewPager();
+        this.startAlarm();
     }
 
     public void historyIconIsClicked (View view){
@@ -39,5 +47,26 @@ public class MainActivity extends AppCompatActivity {
         mViewPager.setAdapter(moodsAdapter);
         mCommentIcon.setBackgroundColor(moodsAdapter.mainActivityIconsColors);
         mHistoryIcon.setBackgroundColor(moodsAdapter.mainActivityIconsColors);
+    }
+
+    private void startAlarm (){
+
+        AlarmManager alarmMgr;
+        PendingIntent alarmIntent;
+        alarmMgr = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(MainActivity.this, AlarmBroadcastReceiver.class);
+        alarmIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, 0);
+        Calendar cal = Calendar.getInstance();
+
+        cal.setTimeInMillis(System.currentTimeMillis());
+        cal.set(Calendar.HOUR_OF_DAY, 8);
+        cal.set(Calendar.MINUTE, 30);
+
+//        alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
+//                1000 * 60 * 20, alarmIntent);
+
+        alarmMgr.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime() + AlarmManager.INTERVAL_FIFTEEN_MINUTES/15,
+                AlarmManager.INTERVAL_FIFTEEN_MINUTES/15, alarmIntent);
     }
 }
---
+++ app/src/main/java/markmomo/com/moodtrackerfinal/Tools/AlarmBroadcastReceiver.java
@@ -0,0 +1,15 @@
+package markmomo.com.moodtrackerfinal.Tools;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        System.out.println("onReceive: onReceive: onReceive: onReceive: onReceive: onReceive: onReceive: ");
+    }
+}
